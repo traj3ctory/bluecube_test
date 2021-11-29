ff8a11e2e0331a4ace09fbdc75c2e71eee2de5d5
traj3ctory <davidbenard.bd@gmail.com>
2021-11-29 10:02:55 +0100

    fix(img): correct image extension

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = (props) => {
   return (
     <div>
       <section className="not_found">
-        <img src="../asset/img/404.png" alt="notfound" className="img-fluid" />
+        <img src="../asset/img/404.gif" alt="notfound" className="img-fluid" />
       </section>
     </div>
   )

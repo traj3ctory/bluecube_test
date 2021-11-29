f02c72cfb96096a36606f42ce4744b585565898b
traj3ctory <davidbenard.bd@gmail.com>
2021-11-29 10:31:41 +0100

    fix(img): replace local image with a link

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = (props) => {
   return (
     <div>
       <section className="not_found">
-        <img src="../asset/img/404.gif" alt="notfound" className="img-fluid" />
+        <img src="https://cdn.dribbble.com/users/2133818/screenshots/13972097/media/55d46b78c6ea8b2396432e2c9af92e3c.gif" alt="notfound" className="img-fluid" />
       </section>
     </div>
   )
